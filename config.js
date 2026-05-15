d235310339002a56677e69b6e7db078568aec890
NoeMendoza-gif <taquitostime1@gmail.com>
2026-05-14 21:16:37 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const {
   PORT = process.env.PORT || 3000,
   
   
-  SALT_ROUND = 10,
+  SALT_ROUNDS = 10,
   
  
   SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || 'clave-secreta-de-desarrollo-2026'
